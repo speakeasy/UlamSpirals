a6c693f4b6255b1de885517409e1967258a01573
SpeaKeasY <speakeasysky@gmail.com>
2017-06-23 06:28:05 +0000

    Its starting to work.

--- primes.py
+++ primes.py
@@ -1,5 +1,12 @@
 MEMORY_MB_MAX = 4096
+import gmpy2
+
+
 class primes:
+    def __init__(self):
+        self._known_primes = [2, 3, 325, 9375, 28178, 450775, 9780504, 1795265022]
+        self._known_primes += [x for x in range(5, 1000, 2) if self.is_prime(x)]
+
     def factor(self, n, p=2):
         s = 0
         d = n - 1
@@ -8,6 +15,45 @@ class primes:
             s += 1
             q *= p
         return s, d // (q // p)
+
+    def _try_composite(self, a, d, n, s):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2 ** i * d, n) == n - 1:
+                return False
+        return True  # n  is definitely composite
+
+    def is_prime(self, n, _precision_for_huge_n=16):
+        if n < 0:
+            return False
+        if n in self._known_primes or n in (0, 1):
+            return True
+        if any((n % p) == 0 for p in self._known_primes):
+            return False
+        d, s = n - 1, 0
+        while not d % 2:
+            d, s = d >> 1, s + 1
+        # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
+        if d < 0:
+            d = d * -1.0
+        if n < 1373653:
+            return not any(self._try_composite(a, d, n, s) for a in (2, 3))
+        if n < 25326001:
+            return not any(self._try_composite(a, d, n, s) for a in (2, 3, 5))
+        if n < 118670087467:
+            if n == 3215031751:
+                return False
+            return not any(self._try_composite(a, d, n, s) for a in (2, 3, 5, 7))
+        if n < 2152302898747:
+            return not any(self._try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
+        if n < 3474749660383:
+            return not any(self._try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
+        if n < 341550071728321:
+            return not any(self._try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
+        # otherwise
+        return not any(self._try_composite(a, d, n, s)
+                       for a in self._known_primes[:_precision_for_huge_n])
 
     def sieve(self, end):
         prime_list = [2, 3]
--- ulamspirals.py
+++ ulamspirals.py
@@ -1,9 +1,45 @@
 from primes import primes
-
+import numpy as np
 
 class ulamspirals:
     primes = None
+    spiral = []
+    size = None
+    start = None
 
-    def __init__(self):
+    def __init__(self, start, size=1024):
+        if size % 2 == 0:
+            size += 1
         self.primes = primes()
+        self.size = size
+        if start < 0:
+            start = 0
+        self.start = start
 
+    def spiral_matrix(self, n):
+        m = [[0x00] * n for i in range(n)]
+        dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+        x, y = 0, -1
+        c = n * n + self.start - 1
+        for i in range(n + n - 1):
+            for j in range((n + n - i) // 2):
+                x += dx[i % 4]
+                y += dy[i % 4]
+                m[x][y] = 0x00
+                if self.primes.is_prime(c):
+                    m[x][y] = 0x01
+                if c == self.start:
+                    m[x][y] = 'X'
+                c -= 1
+
+        for i in m:
+            i.reverse
+        m.reverse
+        return m
+
+u = ulamspirals(1, 20)
+
+m = u.spiral_matrix(u.size)
+
+for row in m:
+    print(*row)
