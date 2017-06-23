835a6377fde09d43a22b962f8d51827e86fbedb9
SpeaKeasY <speakeasysky@gmail.com>
2017-06-23 09:13:12 +0000

    Added plotting with pyplot.

--- ulamspirals.py
+++ ulamspirals.py
@@ -1,5 +1,7 @@
 from primes import primes
+from matplotlib import pyplot as plt
 import numpy as np
+import scipy.misc
 
 class ulamspirals:
     primes = None
@@ -17,29 +19,42 @@ class ulamspirals:
         self.start = start
 
     def spiral_matrix(self, n):
+        f = False
         m = [[0x00] * n for i in range(n)]
         dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
         x, y = 0, -1
-        c = n * n + self.start - 1
+        c = n * n + self.start
+        c = c * 2# + int((c * 0.5))
+        c -= 2
+        if c % 2 == 0:
+            c -= 1
         for i in range(n + n - 1):
             for j in range((n + n - i) // 2):
                 x += dx[i % 4]
                 y += dy[i % 4]
-                m[x][y] = 0x00
+                m[x][y] = 0xff
+                print(c)
                 if self.primes.is_prime(c):
-                    m[x][y] = 0x01
-                if c == self.start:
-                    m[x][y] = 'X'
-                c -= 1
+                    m[x][y] = 0x00
+                #if c == self.start:
+                #    m[x][y] = 'X'
+                c -= 2
+                #if str(c).endswith("5"):
+                #    c -= 2
 
-        for i in m:
-            i.reverse
-        m.reverse
+
+        for i in range(self.size - 1, 0, -1):
+            m[i] = m[i][::-1]
+
+        m = m[::-1]
         return m
 
-u = ulamspirals(1, 20)
+u = ulamspirals(1,20)
 
 m = u.spiral_matrix(u.size)
 
-for row in m:
-    print(*row)
+plt.imshow(m, interpolation="nearest")
+plt.gray()
+plt.show()
+#scipy.misc.toimage(m, str(u.size) + "x" + str(u.size) + "-s" + str(u.start) + ".png")
+
